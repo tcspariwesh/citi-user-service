095bd26367d037dc4aa3916a6770a824e6a0fcb7
tcspariwesh <pariweshg@gmail.com>
2021-10-25 11:45:06 +0530

    post method

--- src/main/java/com/example/user/UserController.java
+++ src/main/java/com/example/user/UserController.java
@@ -1,12 +1,15 @@
 package com.example.user;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user.entity.User;
 
 @RestController
 public class UserController {
 	@PostMapping("/user")
-	void saveUser() {
-		System.out.println("called");
+	void saveUser(@RequestBody User user) {
+		System.out.println("user name=" + user.getName());
 	}
 }
