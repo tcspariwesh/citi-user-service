596355625fd3b17581217af2a434452dcc2d1c9f
tcspariwesh <pariweshg@gmail.com>
2021-10-27 11:37:13 +0530

    added eureka client support

--- src/main/java/com/example/user/UserServiceApplication.java
+++ src/main/java/com/example/user/UserServiceApplication.java
@@ -2,7 +2,8 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//@EnableEurekaClient   //may be needed in older versions
 @SpringBootApplication
 public class UserServiceApplication {
 
