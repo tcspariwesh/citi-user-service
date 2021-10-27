62ee576c738de7aee2bb7bf903c2ae47a89969a1
tcspariwesh <pariweshg@gmail.com>
2021-10-27 12:15:37 +0530

    service call with RestTemplate

--- src/main/java/com/example/user/UserServiceApplication.java
+++ src/main/java/com/example/user/UserServiceApplication.java
@@ -2,7 +2,11 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 //@EnableEurekaClient   //may be needed in older versions
 @SpringBootApplication
 public class UserServiceApplication {
@@ -11,4 +15,8 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
--- src/main/java/com/example/user/service/UserService.java
+++ src/main/java/com/example/user/service/UserService.java
@@ -1,16 +1,27 @@
 package com.example.user.service;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.example.user.Quote;
 import com.example.user.entity.User;
 import com.example.user.repository.UserRepository;
 @Service
 public class UserService implements IUserService {
+	private static final Logger log = LoggerFactory.getLogger(UserService.class);
+	@Autowired
+	RestTemplate restTemplate;
 	@Autowired
 	UserRepository userRepository;
 	@Override
 	public void saveUser(User user) {
+		Quote quote = restTemplate.getForObject(
+				"https://quoters.apps.pcfone.io/api/random", Quote.class);
+		System.out.println(quote.toString());
 		userRepository.save(user);
 		System.out.println(user.getName());
 	}
